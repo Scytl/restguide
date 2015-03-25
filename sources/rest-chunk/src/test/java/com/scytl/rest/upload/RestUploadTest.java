cea279fa7408abda6f4eb9c74133e05569095a2d
Alex Soto <alex.soto@scytl.com>
2015-03-25 15:31:27 +0100

    updates dependencies

--- sources/rest-chunk/src/test/java/com/scytl/rest/upload/RestUploadTest.java
+++ sources/rest-chunk/src/test/java/com/scytl/rest/upload/RestUploadTest.java
@@ -6,7 +6,6 @@ import static org.hamcrest.CoreMatchers.notNullValue;
 import java.net.URI;
 import java.net.URL;
 
-import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.ws.rs.core.UriBuilder;
 
@@ -17,6 +16,7 @@ import org.jboss.arquillian.test.api.ArquillianResource;
 import org.jboss.arquillian.warp.Inspection;
 import org.jboss.arquillian.warp.Warp;
 import org.jboss.arquillian.warp.WarpTest;
+import org.jboss.arquillian.warp.impl.client.observer.OnlyFirstRequest;
 import org.jboss.arquillian.warp.servlet.AfterServlet;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
@@ -30,47 +30,51 @@ import org.junit.runner.RunWith;
 @WarpTest
 public class RestUploadTest {
 
-	@Deployment
-	public static final WebArchive deployment() {
-		return ShrinkWrap
-				.create(WebArchive.class)
-				.addClasses(BoundedInputStream.class, IOUtil.class,
-						P12Resource.class, TemporaryFolder.class)
-				.addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml")
-				.addAsLibraries(
-						Maven.resolver().loadPomFromFile("pom.xml")
-								.resolve("org.glassfish:javax.json")
-								.withTransitivity().as(JavaArchive.class));
-	}
+    @Deployment
+    public static final WebArchive deployment() {
+        return ShrinkWrap
+            .create(WebArchive.class)
+            .addClasses(BoundedInputStream.class, IOUtil.class,
+                P12Resource.class, TemporaryFolder.class)
+            .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml")
+            .addAsLibraries(
+                Maven.resolver().loadPomFromFile("pom.xml")
+                    .resolve("org.glassfish:javax.json")
+                    .withTransitivity().as(JavaArchive.class));
+    }
 
-	@ArquillianResource
-	URL baseUrl;
+    @ArquillianResource
+    URL baseUrl;
 
-	@Test
-	@RunAsClient
-	public void shouldUploadContent() {
-		Warp.initiate(() -> {
+    @Test
+    @RunAsClient
+    public void shouldUploadContent() {
+        Warp.initiate(
+            () -> {
 
-			URI endpoint = UriBuilder.fromPath(baseUrl.toExternalForm())
-					.path(P12Resource.class).path("chunkedUpload").build();
-			given().request().body("This is a test file.".getBytes())
-					.post(endpoint).then().assertThat()
-					.body("uploadId", notNullValue()).and()
-					.body("offset", notNullValue());
+                URI endpoint =
+                    UriBuilder.fromPath(baseUrl.toExternalForm())
+                        .path(P12Resource.class).path("chunkedUpload")
+                        .build();
+                given().request().body("This is a test file.".getBytes())
+                    .post(endpoint).then().assertThat()
+                    .body("uploadId", notNullValue()).and()
+                    .body("offset", notNullValue());
 
-		}).inspect(new Inspection() {
-			private static final long serialVersionUID = 1L;
-			
-			@Inject
-			TemporaryFolder temporaryFolder;
-			
-			@AfterServlet
-			public void assertCreatedFile() {
-				System.out.println(temporaryFolder.getRoot());
-			}
-			
-		});
+            })
+            .inspect(new Inspection() {
+            private static final long serialVersionUID = 1L;
 
-	}
+            @Inject
+            TemporaryFolder temporaryFolder;
+
+            @AfterServlet
+            public void assertCreatedFile() {
+                System.out.println(temporaryFolder.getRoot());
+            }
+
+        });
+
+    }
 
 }
